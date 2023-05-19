d538b75cef826cba8eb75987c61531e8fec2a378
Ryokuman <ryokuman@naver.com>
2023-05-19 17:22:07 +0900

    feature ( component ) : container

---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  flex-shrink: 0;
+`;
+
+export default Container;
