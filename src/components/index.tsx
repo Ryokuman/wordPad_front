ba8d17ef60975a9815cd0505852d6f34716c87ea
Ryokuman <ryokuman@naver.com>
2023-05-19 21:44:11 +0900

    feature ( component ) : add Button

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import Page from "./Page";
 import Container from "./Container";
 import Typo from "./Typo";
+import Button from "./Button";
 
-export { Page, Container, Typo };
+export { Page, Container, Typo, Button };
