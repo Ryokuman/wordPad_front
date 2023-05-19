9847ac9af53a46ea697d0a027bec7f5bfe20f620
Ryokuman <ryokuman@naver.com>
2023-05-19 16:41:14 +0900

    feature ( pages ) : add initial pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import { List, Test, Main } from "./pages";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<div>hello</div>} />
+      <Route index element={<Main />} />
+      <Route path="test" element={<Test />} />
+      <Route path="list" index element={<List />} />
     </Routes>
   );
 }
