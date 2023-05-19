390f164d489016df477d2c8a20924f3e260583a1
Ryokuman <ryokuman@naver.com>
2023-05-20 00:18:36 +0900

    feature ( page ) : add ListPage

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,7 +1,50 @@
-import Page from "@components/Page";
+import { v4 } from "uuid";
+import { useState } from "react";
+import Button from "@components/Button";
+import PageBody from "@components/PageBody";
+
+import { AddBody, AddInput, Word } from "./style";
+import { useAppDispatch, useAppSelector } from "@stores/store";
+import wordsSlice from "@stores/wordsSlice";
 
 function List() {
-  return <div></div>;
+  const [word, setWord] = useState("");
+  const [meaning, setMeaning] = useState("");
+
+  const dispatch = useAppDispatch();
+  const words = useAppSelector((state) => state.word);
+
+  const onAddClick = () => {
+    if (word && meaning) {
+      const newWord: wordType = { id: v4(), name: word, meaning: meaning };
+      dispatch(wordsSlice.actions.addWord(newWord));
+      setWord("");
+      setMeaning("");
+    }
+  };
+
+  const onWordChange: React.ChangeEventHandler<HTMLInputElement> = (e) => setWord(e.target.value);
+  const onMeaningChange: React.ChangeEventHandler<HTMLInputElement> = (e) => setMeaning(e.target.value);
+
+  return (
+    <PageBody margin="0 auto">
+      <AddBody>
+        <AddInput placeholder="word" value={word} onChange={onWordChange} />
+        <AddInput placeholder="meaning" value={meaning} onChange={onMeaningChange} />
+        <Button
+          width="240px"
+          height="40px"
+          fontSize="32px"
+          value="add word"
+          onClick={onAddClick}
+          border="#ff0000 4px solid"
+        />
+      </AddBody>
+      {words.map((e, i) => (
+        <Word key={i} index={i + 1} word={e} />
+      ))}
+    </PageBody>
+  );
 }
 
 export default List;
---
+++ src/pages/List/style.tsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+import { Button, Container, Input, Typo } from "@components/index";
+import { useAppDispatch } from "@stores/store";
+import wordsSlice from "@stores/wordsSlice";
+
+type wordProps = {
+  index: number;
+  word: wordType;
+};
+
+const AddBody = styled(Container)`
+  width: 800px;
+  margin-top: 104px;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 10px;
+`;
+
+const AddInput = styled(Input)`
+  padding-left: 20px;
+  width: 200px;
+  height: 40px;
+  font-size: 32px;
+  &::placeholder {
+    margin-left: 20px;
+  }
+`;
+
+const WordBody = styled(Container)`
+  flex-direction: row;
+  justify-content: flex-start;
+  width: 780px;
+  margin-top: 10px;
+`;
+
+const Index = styled(Typo)`
+  width: 60px;
+  font-size: 24px;
+  white-space: nowrap;
+`;
+const WordName = styled(Typo)`
+  width: 240px;
+  font-size: 24px;
+  white-space: nowrap;
+`;
+const Meaning = styled(Typo)`
+  font-size: 24px;
+  white-space: nowrap;
+`;
+
+function Word({ word, index }: wordProps) {
+  const dispatch = useAppDispatch();
+
+  const onDelClick = () => dispatch(wordsSlice.actions.deleteWord(word));
+
+  return (
+    <WordBody>
+      <Index>{index}.</Index>
+      <WordName>{word.name}</WordName>
+      <Meaning>{word.meaning}</Meaning>
+      <Button
+        margin="auto 0 auto auto"
+        border="#ff0000 2px solid"
+        width="20px"
+        height="20px"
+        value="x"
+        fontSize="10px"
+        onClick={onDelClick}
+      />
+    </WordBody>
+  );
+}
+
+export { AddBody, AddInput, Word };
